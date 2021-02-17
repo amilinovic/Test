424f0644495d1665b6bf7c97bb8bebc872fbc1e0
Andjelko Milinovic <amilinovic@execom.eu>
2021-02-17 18:32:33 +0100

    Test

--- ci_cd/build_release.py
+++ ci_cd/build_release.py
@@ -13,12 +13,14 @@ print(newfileslist)
 for addedfile in newfileslist:
   name_directory = os.path.splitext(addedfile)[0].split("/")   #addedfile.decode("utf-8").split("/")[0]
   directory = name_directory[0]
+  project_name = name_directory[1]
   # print(directory)
   # run_databrick_commands = True
 
   if directory == 'databricks':
     print(directory + ' Added file for databricks ' + addedfile)
-    print('##vso[task.setvariable variable=projectName;isOutput=true;]'project_name)
+    # print('$(project_name)')
+    print('##vso[task.setvariable variable=project_name;]%s' % (project_name))
     
   else:
     print('No added files to databricks. Exiting.')
