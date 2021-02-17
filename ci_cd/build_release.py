6ca88e5d3d60c0df730132b88ee19e6dd32346b0
Andjelko Milinovic <amilinovic@execom.eu>
2021-02-17 16:26:02 +0100

    test

--- ci_cd/build_release.py
+++ ci_cd/build_release.py
@@ -8,12 +8,13 @@ print('Just a test of script')
 
 files = subprocess.check_output('git diff --diff-filter=AMR --name-only HEAD^ HEAD',shell=True)
 
-newfiles = files.split("/")[0]
+newfiles = files.split("/")
+print(newfiles)
 
-if newfiles == "darabricks":
-  print(newfiles + "Added file for databricks")
-else:
-  print("Added file for marlinkpy")
+# if newfiles == "darabricks":
+#   print(newfiles + "Added file for databricks")
+# else:
+#   print("Added file for marlinkpy")
 
 # Moze da se doda da ako je editovaj databricks folder da pokrene ovaj pipeline. A ako je editovan marlinkpy da pokrene drugi pipeline
 # if newfiles.decode("utf-8") == '':
