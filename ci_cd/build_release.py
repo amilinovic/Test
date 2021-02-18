396240d7542d21cd9ec190aed760017c26ab8d0e
Andjelko Milinovic <amilinovic@execom.eu>
2021-02-18 14:12:27 +0100

    Test template

--- ci_cd/build_release.py
+++ ci_cd/build_release.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-# import sys
 import os
 import shutil
 import subprocess
 import json
 
+# Check git difference
 newfiles = subprocess.check_output('git diff --diff-filter=AMR --name-only HEAD^ HEAD',shell=True)
-# subprocess.check_call([sys.executable, '-m', 'pip3', 'install', 'databricks-cli'])
+
 original = "/home/vsts/work/1/s"
 target = "/home/vsts/work/1/databricks"
 
@@ -17,16 +17,14 @@ for addedfile in newfileslist:
   name_directory = os.path.splitext(addedfile)[0].split("/")   #addedfile.decode("utf-8").split("/")[0]
   directory = name_directory[0]
   project_name = name_directory[1]
-  # print(directory)
-  # run_databrick_commands = True
 
   if directory == 'databricks':
     print(directory + ' Added file for databricks ' + addedfile)
-    # print('$(project_name)')
+    # Copy files to databricks directory
     shutil.copytree(original, target)
+    # Pass variables from script to azure devops pipeline
     print('##vso[task.setvariable variable=directory;]%s' % (directory))
     print('##vso[task.setvariable variable=project_name;]%s' % (project_name))
-    # print("Current working dir : %s" % os.getcwd())          /home/vsts/work/1/s
-    
+
   else:
     print('No added files to databricks. Exiting.')
