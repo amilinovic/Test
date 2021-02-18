ca7191e1cb00b4a0f3661cd0a3995d2cf72ae8c5
Andjelko Milinovic <amilinovic@execom.eu>
2021-02-18 15:59:23 +0100

    Test script

--- ci_cd/build_release.py
+++ ci_cd/build_release.py
@@ -11,13 +11,6 @@ newfiles = subprocess.check_output('git diff --diff-filter=AMR --name-only HEAD^
 original = "/home/vsts/work/1/s"
 target = "/home/vsts/work/1/databricks"
 
-# if os.path.isdir(target) == True:
-# os.remove(target)
-  # shutil.copytree(original, target)
-#   print('Deleted directory')
-# else:
-#   print('Did not delete the directory')
-
 newfileslist = newfiles.decode("utf-8").splitlines()
 
 for addedfile in newfileslist:
@@ -25,11 +18,16 @@ for addedfile in newfileslist:
   directory = name_directory[0]
   project_name = name_directory[1]
 
+  if os.path.isdir(target) == True:
+    os.rmdir(target)
+  else:
+    print(target + ' Dir not created still')
+
   if directory == 'databricks':
     print(directory + ' Added file for databricks ' + addedfile)
 
     # Copy files to databricks directory
-    shutil.copyfile(addedfile, target)
+    shutil.copytree(original, target)
 
     # Pass variables from script to azure devops pipeline
     print('##vso[task.setvariable variable=directory;]%s' % (directory))
