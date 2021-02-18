ac070d628adc5dbb07ef268e6096cb80df10c56a
Andjelko Milinovic <amilinovic@execom.eu>
2021-02-18 15:12:50 +0100

    Test of python script

--- ci_cd/build_release.py
+++ ci_cd/build_release.py
@@ -14,14 +14,20 @@ target = "/home/vsts/work/1/databricks"
 newfileslist = newfiles.decode("utf-8").splitlines()
 
 for addedfile in newfileslist:
-  name_directory = os.path.splitext(addedfile)[0].split("/")   #addedfile.decode("utf-8").split("/")[0]
+  name_directory = os.path.splitext(addedfile)[0].split("/")
+  # Get directory name
   directory = name_directory[0]
+  # Get project name
   project_name = name_directory[1]
 
   if directory == 'databricks':
     print(directory + ' Added file for databricks ' + addedfile)
-    # Copy files to databricks directory
-    shutil.copytree(original, target)
+    if os.path.isdir(target) == True:
+      os.rmdir(target)
+      shutil.copytree(original, target)
+    else:
+      # Copy files to databricks directory
+      shutil.copytree(original, target)
     # Pass variables from script to azure devops pipeline
     print('##vso[task.setvariable variable=directory;]%s' % (directory))
     print('##vso[task.setvariable variable=project_name;]%s' % (project_name))
