e23d6695672e7a8024245e03567a4280dc3f6b33
Andjelko Milinovic <amilinovic@execom.eu>
2021-02-18 16:23:00 +0100

    Rename of projects, and added some comments

--- ci_cd/build_release.py
+++ ci_cd/build_release.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
 
 import os
-import shutil
 import subprocess
-import json
 
 # Check git difference
 newfiles = subprocess.check_output('git diff --diff-filter=AMR --name-only HEAD^ HEAD',shell=True)
 
-original = "/home/vsts/work/1/s"
-target = "/home/vsts/work/1/databricks"
-
+# Added files to list
 newfileslist = newfiles.decode("utf-8").splitlines()
 
 for addedfile in newfileslist:
   name_directory = os.path.splitext(addedfile)[0].split("/")
+  # Get name of his directory
   directory = name_directory[0]
+  # Get a name of project
   project_name = name_directory[1]
 
-  # if os.path.isdir(target) == True:
-  #   os.rmdir(target)
-  # else:
-  #   print(target + ' Dir not created still')
-
   if directory == 'databricks':
-    print(directory + ' Added file for databricks ' + addedfile)
-
-    # Copy files to databricks directory
-    # shutil.copytree(original, target)
+    print(' Added file for databricks is: ' + addedfile)
 
     # Pass variables from script to azure devops pipeline
     print('##vso[task.setvariable variable=directory;]%s' % (directory))
     print('##vso[task.setvariable variable=project_name;]%s' % (project_name))
 
   else:
-    print('No added files to databricks. Exiting.')
+    print('No added files to databricks dir.')
