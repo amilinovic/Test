1bfeee2629b46238be63e3dd5fb7bb21c2b7f7c5
Andjelko Milinovic <amilinovic@execom.eu>
2021-04-20 10:21:12 +0200

    Test changes

--- marlinkpy/marlinkpy/utility.py
+++ marlinkpy/marlinkpy/utility.py
@@ -118,7 +118,7 @@ def usql_to_pyspark_script(query_string, column_names):
     print(query_string)
 
 def hello():
-    print("Hello World #1")
+    print("Hello World #3")
 
 def hello2():
-    print("Hello World #2")
+    print("Hello World #4")
