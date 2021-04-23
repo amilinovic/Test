7bace8281ffb932fc9e3b75114b4827f9ef7dcb3
Andjelko Milinovic <amilinovic@execom.eu>
2021-04-23 11:18:06 +0200

    Chanve script to bash script

--- marlinkpy/lib-script/test.sh
+++ marlinkpy/lib-script/test.sh
@@ -1,11 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/bash
 
-import subprocess
-import wheel
-import setuptools
-import twine
+# import subprocess
+# import wheel
+# import setuptools
+# import twine
 
 cd /home/vsts/work/1/s/marlinkpy
+source venv/Scripts/activate
+pip3 install wheel
+pip3 install setuptools
+pip3 install twine
 python3 setup.py sdist bdist_wheel
 
 var = subprocess.check_output('git diff --name-only --diff-filter=AMR HEAD^1 HEAD | xargs -I '{}' cp --parents -r '{}' $(Build.BinariesDirectory)')
