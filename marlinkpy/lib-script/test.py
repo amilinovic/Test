7a0e2b64cdda6947751cbf8e019db46ac6bf8023
Andjelko Milinovic <amilinovic@execom.eu>
2021-04-23 11:08:10 +0200

    Test python script

--- marlinkpy/lib-script/test.py
+++ marlinkpy/lib-script/test.py
@@ -1,12 +1,15 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 
-# import subprocess
+import subprocess
+import wheel
+import setuptools
+import twine
 
 cd /home/vsts/work/1/s/marlinkpy
 # source venv/Scripts/activate
-pip3 install wheel
-pip3 install setuptools
-pip3 install twine
+# pip3 install wheel
+# pip3 install setuptools
+# pip3 install twine
 python3 setup.py sdist bdist_wheel
 
 var = subprocess.check_output('git diff --name-only --diff-filter=AMR HEAD^1 HEAD | xargs -I '{}' cp --parents -r '{}' $(Build.BinariesDirectory)')
