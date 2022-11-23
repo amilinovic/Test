ed40b2d597dbe3aaaebad10e685732280bef3faf
Andjelko Milinovic <andjelko.milinovic@htecgroup.com>
2022-11-23 14:09:06 +0100

    Test

---
+++ ci_cd/databricks/templates/new_build_release.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import shutil 
+from databricks_cli.sdk.api_client import ApiClient
+from databricks_cli.workspace.api import WorkspaceApi, WorkspaceFileInfo
+
+api_client = ApiClient(
+  host  = os.getenv('DATABRICKS_HOST'),
+  token = os.getenv('DATABRICKS_TOKEN')
+)
+
+# Check git difference
+newfiles = subprocess.check_output('git diff --diff-filter=AMR --name-only HEAD^ HEAD',shell=True)
+
+print(newfiles)
+
+# Added files to list
+newfileslist = newfiles.decode("utf-8").splitlines()
+print(newfileslist)
+
+# Source path
+src = '/home/vsts/work/1/s'
+# Destination path
+dest = '/home/vsts/work/1/d/'
+files = []
+
+for addedfile in newfileslist:
+  name_directory = os.path.splitext(addedfile)[0].split("/")
+  # Get name of his directory
+  directory = name_directory[0]
+  # Get a name of project
+  project_name = name_directory[1]
+  print(name_directory)
+  playbook_name = name_directory[2]
+
+  if directory == 'databricks':
+    print(' Added file for databricks is: ' + addedfile)
+    # files = addedfile[11:]
+    workspace_api = WorkspaceApi(api_client)
+    workspace_import = workspace_api.import_workspace(
+      source_path= addedfile[11:], 
+      target_path= "/Tst/Test/",
+      language= "PYTHON",
+      overwrite= "true"
+    )
+
+
+
+
+    # files.append(addedfile[11:])
+    # print(files)
+    # if os.path.exists(dest):
+    #   pass
+    # else:
+    #   shutil.copytree(src, dest)
+
+  #   # Pass variables from script to azure devops pipeline
+  #   print('##vso[task.setvariable variable=directory;]%s' % (directory))
+  #   print('##vso[task.setvariable variable=project_name;]%s' % (project_name))
+  #   print('##vso[task.setvariable variable=playbook_name;]%s' % (playbook_name))
+  # print('##vso[task.setvariable variable=files;]%s' % (files))
+
+  # else:
+  #   print('No added files to databricks dir.')
+
+
+# Try to get name of playbook (playbook_name = name_directory[2]) and pass that name from script to azure devops pipeline. And add it to databricks workspace import_dir task in azure_release_pipeline.yml
