7d4b3e085502a23be9c341bc5acc92c76271e0ab
Andjelko Milinovic <andjelko.milinovic@htecgroup.com>
2022-11-29 15:09:31 +0100

    Test

--- ci_cd/databricks/templates/new_build_release.py
+++ ci_cd/databricks/templates/new_build_release.py
@@ -12,46 +12,56 @@ api_client = ApiClient(
 )
 
 # Check git difference
-newfiles = subprocess.check_output('git diff --diff-filter=AMR --name-only HEAD^ HEAD',shell=True)
+newfiles = subprocess.check_output('git diff --diff-filter=AM --name-only HEAD^ HEAD',shell=True)
+removedfiles = subprocess.check_output('git diff --diff-filter=R --name-only HEAD^ HEAD',shell=True)
+
+print(newfiles)
+print(removedfiles)
 
 # print(newfiles)
 
 # Added files to list
-newfileslist = newfiles.decode("utf-8").splitlines()
-# print(newfileslist)
+# newfileslist = newfiles.decode("utf-8").splitlines()
+# # print(newfileslist)
 
-# # Source path
-src = '/home/vsts/work/1/s'
-# # Destination path
-# os.mkdir('/tmp/notebooks')
-# dest = '/tmp/notebooks'
+# # # Source path
+# src = '/home/vsts/work/1/s'
+# # # Destination path
+# # os.mkdir('/tmp/notebooks')
+# # dest = '/tmp/notebooks'
 
-for addedfile in newfileslist:
-  name_directory = os.path.splitext(addedfile)[0].split("/")
-  # # Get name of his directory
-  directory = name_directory[0]
-  # # Get a name of project
-  # project_name = name_directory[1]
-  # print(name_directory)
+# for addedfile in newfileslist:
+#   name_directory = os.path.splitext(addedfile)[0].split("/")
+#   # # Get name of his directory
+#   directory = name_directory[0]
+#   # # Get a name of project
+#   # project_name = name_directory[1]
+#   # print(name_directory)
 
-  if directory == 'databricks':
-    print(' Added file for databricks is: ' + addedfile)
-    # Create workspace path for creating directory in Databricks
-    workspace = name_directory[1:-1]
-    workspace_path = "/".join(workspace)
-    # Create target path for adding notebook to Databricks
-    target = name_directory[1:]
-    target_path = "/".join(target)
-# Tst must be env variable
-    workspace_api = WorkspaceApi(api_client)
-    workspace_directory = workspace_api.mkdirs(workspace_path = "/Tst/"+workspace_path)
-    workspace_import = workspace_api.import_workspace(
-      source_path = src+"/"+addedfile,
-      target_path = "/Tst/"+target_path,
-      is_overwrite = "true",
-      fmt = "SOURCE",
-      language = "PYTHON"
-      )
+#   if directory == 'databricks':
+#     print(' Added file for databricks is: ' + addedfile)
+#     # Create workspace path for creating directory in Databricks
+#     workspace = name_directory[1:-1]
+#     workspace_path = "/".join(workspace)
+#     # Create target path for adding notebook to Databricks
+#     target = name_directory[1:]
+#     target_path = "/".join(target)
+# # Tst must be env variable
+#     workspace_api = WorkspaceApi(api_client)
+#     workspace_directory = workspace_api.mkdirs(workspace_path = "/Tst/"+workspace_path)
+#     workspace_import = workspace_api.import_workspace(
+#       source_path = src+"/"+addedfile,
+#       target_path = "/Tst/"+target_path,
+#       is_overwrite = "true",
+#       fmt = "SOURCE",
+#       language = "PYTHON"
+#       )
+
+
+
+
+
+
 #     else:
 #       workspace_api = WorkspaceApi(api_client)
 #       workspace_directory = workspace_api.mkdirs(workspace_path = "/Tst/"+project_name)
