b4b546f1110f85d6894f99f4b55b64d161c230e5
Andjelko Milinovic <andjelko.milinovic@htecgroup.com>
2022-11-29 11:04:03 +0100

    Test

--- ci_cd/databricks/templates/new_build_release.py
+++ ci_cd/databricks/templates/new_build_release.py
@@ -21,39 +21,43 @@ newfileslist = newfiles.decode("utf-8").splitlines()
 print(newfileslist)
 
 # # Source path
-# src = '/home/vsts/work/1/s'
+src = '/home/vsts/work/1/s'
 # # Destination path
 # os.mkdir('/tmp/notebooks')
 # dest = '/tmp/notebooks'
 
 for addedfile in newfileslist:
   name_directory = os.path.splitext(addedfile)[0].split("/")
-  # Get name of his directory
-  directory = name_directory[0]
-  # Get a name of project
-  project_name = name_directory[1]
-  print(name_directory)
+  files = addedfile[1:-1]
+  path = "/".join(files)
+  print(files)
+  print(path)
+  # # Get name of his directory
+  # directory = name_directory[0]
+  # # Get a name of project
+  # project_name = name_directory[1]
+  # print(name_directory)
 
-  if directory == 'databricks':
-    print(' Added file for databricks is: ' + addedfile)
-# Tst must be env variable
-    if len(name_directory) == 4:
-      workspace_api = WorkspaceApi(api_client)
-      workspace_directory = workspace_api.mkdirs(workspace_path = "/Tst/"+project_name+"/"+name_directory[2])
-      workspace_import = workspace_api.import_workspace(
-        source_path = src+"/"+addedfile,
-        target_path = "/Tst/"+project_name+"/"+name_directory[2]+"/"+name_directory[3],
-        is_overwrite = "true",
-        fmt = "SOURCE",
-        language = "PYTHON"
-      )
-    else:
-      workspace_api = WorkspaceApi(api_client)
-      workspace_directory = workspace_api.mkdirs(workspace_path = "/Tst/"+project_name)
-      workspace_import = workspace_api.import_workspace(
-        source_path = src+"/"+addedfile,
-        target_path = "/Tst/"+project_name+"/"+name_directory[2],
-        is_overwrite = "true",
-        fmt = "SOURCE",
-        language = "PYTHON"
-      )
+#   if directory == 'databricks':
+#     print(' Added file for databricks is: ' + addedfile)
+# # Tst must be env variable
+#     if len(name_directory) == 4:
+#       workspace_api = WorkspaceApi(api_client)
+#       workspace_directory = workspace_api.mkdirs(workspace_path = "/Tst/"+project_name+"/"+name_directory[2])
+#       workspace_import = workspace_api.import_workspace(
+#         source_path = src+"/"+addedfile,
+#         target_path = "/Tst/"+project_name+"/"+name_directory[2]+"/"+name_directory[3],
+#         is_overwrite = "true",
+#         fmt = "SOURCE",
+#         language = "PYTHON"
+#       )
+#     else:
+#       workspace_api = WorkspaceApi(api_client)
+#       workspace_directory = workspace_api.mkdirs(workspace_path = "/Tst/"+project_name)
+#       workspace_import = workspace_api.import_workspace(
+#         source_path = src+"/"+addedfile,
+#         target_path = "/Tst/"+project_name+"/"+name_directory[2],
+#         is_overwrite = "true",
+#         fmt = "SOURCE",
+#         language = "PYTHON"
+#       )
