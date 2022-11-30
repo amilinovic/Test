bec4137b9fabcb6544072333621ac3d419dabb1c
Andjelko Milinovic <andjelko.milinovic@htecgroup.com>
2022-11-30 14:47:44 +0100

    Test

--- ci_cd/databricks/templates/new_build_release.py
+++ ci_cd/databricks/templates/new_build_release.py
@@ -2,9 +2,9 @@
 
 import os
 import subprocess
-import shutil 
+import shutil
 from databricks_cli.sdk.api_client import ApiClient
-from databricks_cli.workspace.api import WorkspaceApi, WorkspaceFileInfo
+from databricks_cli.workspace.api import WorkspaceApi
 
 api_client = ApiClient(
   host  = os.getenv('DATABRICKS_HOST'),
@@ -27,7 +27,7 @@ def add_files():
     name_directory = os.path.splitext(added_file)[0].split("/")
     # Get name of his directory
     directory = name_directory[0]
-
+    # If file is in databricks directory on repo
     if directory == 'databricks':
       print(' Added file for databricks is: ' + added_file)
       # Create workspace path for creating directory in Databricks
@@ -36,9 +36,10 @@ def add_files():
       # Create target path for adding notebook to Databricks
       target = name_directory[1:]
       target_path = "/".join(target)
-      # Tst must be env variable
       workspace_api = WorkspaceApi(api_client)
+      # Create directory in databricks if not exists
       workspace_directory = workspace_api.mkdirs(workspace_path = "/"+os.getenv('ENV')+"/"+workspace_path)
+      # Import Notebook to Databricks
       workspace_import = workspace_api.import_workspace(
         source_path = src+"/"+added_file,
         target_path = "/"+os.getenv('ENV')+"/"+target_path,
@@ -46,18 +47,21 @@ def add_files():
         fmt = "SOURCE",
         language = "PYTHON"
         )
+  return ""
 
 def remove_files():
   for removed_file in removefileslist:
     name_directory = os.path.splitext(removed_file)[0].split("/")
     # Get name of his directory
     directory = name_directory[0]
-    notebook = name_directory[1:]
-    delete_notebook = "/".join(notebook)
-
+    # If file is in databricks directory on repo
     if directory == 'databricks':
       print(' Removed file from databricks is: ' + removed_file)
+      # Create path of a notebook to delete it from Databricks
+      notebook = name_directory[1:]
+      delete_notebook = "/".join(notebook)
       workspace_api = WorkspaceApi(api_client)
+      # Remove notebook from Databricks
       notebook_delete = workspace_api.delete(
         workspace_path = "/"+os.getenv('ENV')+"/"+delete_notebook,
         is_recursive = "false"
@@ -65,64 +69,3 @@ def remove_files():
 
 print(add_files())
 print(remove_files())
-
-
-
-
-
-
-
-# print(newfiles)
-
-# Added files to list
-# newfileslist = newfiles.decode("utf-8").splitlines()
-# # print(newfileslist)
-
-# # # Source path
-# src = '/home/vsts/work/1/s'
-# # # Destination path
-# # os.mkdir('/tmp/notebooks')
-# # dest = '/tmp/notebooks'
-
-# for addedfile in newfileslist:
-#   name_directory = os.path.splitext(addedfile)[0].split("/")
-#   # # Get name of his directory
-#   directory = name_directory[0]
-#   # # Get a name of project
-#   # project_name = name_directory[1]
-#   # print(name_directory)
-
-#   if directory == 'databricks':
-#     print(' Added file for databricks is: ' + addedfile)
-#     # Create workspace path for creating directory in Databricks
-#     workspace = name_directory[1:-1]
-#     workspace_path = "/".join(workspace)
-#     # Create target path for adding notebook to Databricks
-#     target = name_directory[1:]
-#     target_path = "/".join(target)
-# # Tst must be env variable
-#     workspace_api = WorkspaceApi(api_client)
-#     workspace_directory = workspace_api.mkdirs(workspace_path = "/Tst/"+workspace_path)
-#     workspace_import = workspace_api.import_workspace(
-#       source_path = src+"/"+addedfile,
-#       target_path = "/Tst/"+target_path,
-#       is_overwrite = "true",
-#       fmt = "SOURCE",
-#       language = "PYTHON"
-#       )
-
-
-
-
-
-
-#     else:
-#       workspace_api = WorkspaceApi(api_client)
-#       workspace_directory = workspace_api.mkdirs(workspace_path = "/Tst/"+project_name)
-#       workspace_import = workspace_api.import_workspace(
-#         source_path = src+"/"+addedfile,
-#         target_path = "/Tst/"+project_name+"/"+name_directory[2],
-#         is_overwrite = "true",
-#         fmt = "SOURCE",
-#         language = "PYTHON"
-#       )
